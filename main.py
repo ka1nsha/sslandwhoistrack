9d4c459e6a28875b707ba878a1b86face4066b7d
Enes Ergün <02484614@ksnet.local>
2019-09-16 15:19:36 +0300

    Whois sorgusu için ana şema eklendi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
-cert = CertInfo(hostname="enesergun.net")
-cert.connect()
-print(cert.return_json)
+from modules.sslcheck import *
+from modules.whoischeck import *
+#cert = CertInfo(hostname="enesergun.net")
+#cert.connect()
+#print(cert.return_json)
+wh = DomainQuery(domain='https://enesergun.net')
+print(wh.connect_whois_server)
